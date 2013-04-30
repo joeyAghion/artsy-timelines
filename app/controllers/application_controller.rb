e6b123efd6a94631bc2bbd2f48522735455b612f
Joey Aghion <joey@aghion.com>
2013-04-30 12:06:50 -0400

    Redirect to an enforced hostname (e.g., timelines.artsy.net) if configured.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :check_hostname
+  
   helper_method :artsy
   
   
   protected
+  
+  def check_hostname
+    return unless Figaro.env.respond_to?(:hostname) && Figaro.env.hostname.present?
+    redirect_to (request.protocol + Figaro.env.hostname + request.fullpath) if request.host_with_port != Figaro.env.hostname
+  end
   
   def artsy
     @artsy ||= Artsy::Client.new xapp_token: Figaro.env.artsy_xapp_token
